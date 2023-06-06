eb1c2279b137eb8b5da46617f78c550edaf8fba6
lucascarvalholopes <lucascarvalholopes2@gmail.com>
2023-06-06 12:30:02 -0300

    atauliazação

---
+++ Acadium-back/src/models/Reserve.ts
@@ -0,0 +1,26 @@
+import mongoose, { Types } from "mongoose";
+
+const ReserveSchema = new mongoose.Schema({
+    id_user: {
+        require: true,
+        type: Types.ObjectId,
+        ref: 'user'
+    },
+    id_room: {
+        require: true,
+        type: Types.ObjectId,
+        ref: 'user'
+    },
+    initial_date: {
+        require: true,
+        type: Date,
+    },
+    final_date: {
+        require: true,
+        type: Date,
+    }
+})
+
+const ReserveModel = mongoose.model('reserve',ReserveSchema);
+
+export default ReserveModel;
